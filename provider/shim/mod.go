40cf25ed5a914d993eef30e34c0dabf9a95eeacd
WANG Xuerui <git@xen0n.name>
2020-03-28 13:22:55 +0800

    refactor: move foo -> provider/shim and rename type

--- provider/shim/mod.go
+++ provider/shim/mod.go
@@ -1,52 +1,52 @@
-package foo
+package shim
 
 import (
 	cloudprovider "k8s.io/cloud-provider"
 )
 
-type foo struct{}
+type CloudProvider struct{}
 
-var _ cloudprovider.Interface = foo{}
+var _ cloudprovider.Interface = (*CloudProvider)(nil)
 
 // Initialize provides the cloud with a kubernetes client builder and may spawn goroutines
 // to perform housekeeping or run custom controllers specific to the cloud provider.
 // Any tasks started here should be cleaned up when the stop channel closes.
-func (p foo) Initialize(clientBuilder cloudprovider.ControllerClientBuilder, stop <-chan struct{}) {
+func (p *CloudProvider) Initialize(clientBuilder cloudprovider.ControllerClientBuilder, stop <-chan struct{}) {
 	// TODO: currently no-op
 }
 
 // LoadBalancer returns a balancer interface. Also returns true if the interface is supported, false otherwise.
-func (p foo) LoadBalancer() (cloudprovider.LoadBalancer, bool) {
+func (p *CloudProvider) LoadBalancer() (cloudprovider.LoadBalancer, bool) {
 	return nil, false
 }
 
 // Instances returns an instances interface. Also returns true if the interface is supported, false otherwise.
-func (p foo) Instances() (cloudprovider.Instances, bool) {
+func (p *CloudProvider) Instances() (cloudprovider.Instances, bool) {
 	return nil, false
 }
 
 // Zones returns a zones interface. Also returns true if the interface is supported, false otherwise.
-func (p foo) Zones() (cloudprovider.Zones, bool) {
+func (p *CloudProvider) Zones() (cloudprovider.Zones, bool) {
 	return nil, false
 }
 
 // Clusters returns a clusters interface.  Also returns true if the interface is supported, false otherwise.
-func (p foo) Clusters() (cloudprovider.Clusters, bool) {
+func (p *CloudProvider) Clusters() (cloudprovider.Clusters, bool) {
 	return nil, false
 }
 
 // Routes returns a routes interface along with whether the interface is supported.
-func (p foo) Routes() (cloudprovider.Routes, bool) {
+func (p *CloudProvider) Routes() (cloudprovider.Routes, bool) {
 	return nil, false
 }
 
 // ProviderName returns the cloud provider ID.
-func (p foo) ProviderName() string {
+func (p *CloudProvider) ProviderName() string {
 	// TODO: return something that can be configured
 	return "shim"
 }
 
 // HasClusterID returns true if a ClusterID is required and set
-func (p foo) HasClusterID() bool {
+func (p *CloudProvider) HasClusterID() bool {
 	return true
 }
